8f4fdbba319db331e580f5a81072389870a8d7ba
nguyentienanhvuongchuse <nguyenvuong.09062000@gmail.com>
2020-11-18 09:58:18 +0700

    update location

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -4,4 +4,25 @@ class Admin::LocationsController < ApplicationController
                          .paginate(page: params[:page],
                             per_page: Settings.paginate.manage)
   end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to admin_locations_path
+      flash[:success] = t "message.update_location"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location)
+          .permit(:name, :phone, :address, :distric, :description)
+  end
 end
